2286222c6d7c1ffb95a21e0c6eee8e807a79d07c
Kinsey <kinseybasinger@gmail.com>
2020-05-11 17:42:31 -0500

    initial

---
+++ treehouse_/resources/views/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document for faster load -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="{{asset('js/app.js')}}"></script>
---
+++ treehouse_/resources/views/treehouse.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+@include('head')
+<body style="background-color: #edeff0 !important">
+@include('nav')
+<main role="main" class="container">
+    @yield('body')
+</main><!-- /.container -->
+@include('footer')
+</body>
+</html>
